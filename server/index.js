b92f6fb913cd43ff0cb64af5f502b408160b28a8
André Wolden <andre.wolden@bekk.no>
2018-08-05 16:21:25 +0200

    fix bundling

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const port = process.env.PORT ? process.env.PORT : 1337;
 
 
 app = express();
 
-app.use(express.static('static'));
-app.use(express.static('dist'));
+app.use(express.static(__dirname));
+// app.use(express.static('dist'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 
 app.listen(port, () =>{
     console.log(`all-by-myself listening on port ${port}`)
