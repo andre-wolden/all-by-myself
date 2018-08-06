967d08eaf7eba60168dc38ab864092bcaee2d775
André Wolden <andre.wolden@bekk.no>
2018-08-06 07:30:51 +0200

    akjsfn

--- src/index.js
+++ src/index.js
@@ -4,34 +4,10 @@ console.warn("Two plus two equals = ");
 console.warn(value);
 
 
-import React from 'react';
 import ReactDOM from 'react-dom';
-
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            liked: false
-        };
-    }
-
-    render(){
-
-        if (this.state.liked) {
-            return 'You liked this.';
-        }
-
-        return e(
-            'button',
-            { onClick: () => this.setState({liked: true})},
-            'Like'
-        );
-    }
-}
+import LikeButton from 'main.jsx';
 
 
 const domContainer = document.querySelector('#root');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(LikeButton, domContainer);
 
